e40e1946ec778bda0fb2b47dc61aae667b7e24f5
Noah Maurer <noahmaurer12@gmail.com>
2023-02-07 13:17:27 -0600

    ready to merge

--- flexed/src/Nav.js
+++ flexed/src/Nav.js
@@ -2,7 +2,7 @@ import "./Nav.css";
 
 function NavBar() {
     return (
-        <nav className="navbar navbar-expand-md navbar-dark">
+        <nav className="navbar navbar-expand-md bg-dark">
             <div className="container-fluid">
                 <div className="navbar-brand" to="/">
                     <img
