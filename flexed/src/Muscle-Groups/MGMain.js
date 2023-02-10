33b80e2889c046fafc434ae8485202c22c3f6021
Noah Maurer <noahmaurer12@gmail.com>
2023-02-09 18:32:48 -0600

    up to date

--- flexed/src/Muscle-Groups/MGMain.js
+++ flexed/src/Muscle-Groups/MGMain.js
@@ -2,6 +2,22 @@ import { useGetMuscleGroupsQuery } from "../flexedApi";
 
 export default function MGMain() {
     const { data, error, isLoading } = useGetMuscleGroupsQuery();
-    // console.log(data);
-    return <></>;
+
+    return (
+        <>
+            {isLoading === true ? (
+                <div className="d-flex justify-content-center">
+                    <div className="spinner-border" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </div>
+                </div>
+            ) : (
+                <div>
+                    {data?.muscle_groups.map((mg) => {
+                        return <div key={mg.id}>{mg.name}</div>;
+                    })}
+                </div>
+            )}
+        </>
+    );
 }
